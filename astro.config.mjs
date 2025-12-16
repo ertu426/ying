856d525cd1ef67630520763cd64c5f350d83f642
ertu426 <erxat124@gmail.com>
2025-12-17 02:55:27 +0800

    feat(site): Complete the homepage theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import UnoCSS from 'unocss/astro'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'http://localhost:4321',
+    integrations: [
+        mdx(), 
+        sitemap(),
+        UnoCSS({
+            injectReset: true
+        }),
+    ],
+});
