b01848d3f4a55a068ae9a132d6009d7edef24483
ertu426 <erxat124@gmail.com>
2025-12-24 19:56:53 +0800

    feat: 更新网站配置并添加C++教程文章
    更新网站标题、描述和备案信息
    删除旧的占位图片并添加新的教程图片
    新增两篇C++教程文章

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
-export const HITOKOTO_ENABLLE = false;
+export const SITE_TITLE = "京域侠客行";
+export const SITE_DESCRIPTION = "不想当游戏设计师的程序员不是一个好厨子!";
+export const HITOKOTO_ENABLLE = true;
 export const HITOKOTO_TEXT = "Practice makes perfect.";
 
-export const BEIAN_NUMBER = "京ICP备2025000000号-1";
+export const BEIAN_NUMBER = "津ICP备2022007019号-1";
 export const GONGAN_NUMBER = "京公网安备00000000000000号";
